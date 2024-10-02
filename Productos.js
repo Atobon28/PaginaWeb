347c94c4c6784651830cd0780e1674bf37b12d85
Ana Tobon <anita.tobon28@gmail.com>
2024-10-02 07:46:07 -0500

    JS 2

--- Productos.js
+++ Productos.js
@@ -1,130 +1,146 @@
 let map = { 
     Cafes: { 
         cafe1: { 
-            nombre: "Maragogipe" 
-            descripcion: "Desde las costas montañosas de Tumaco, Nariño, el Café Finca Los Negros ofrece un distinguido Maragogipe. Este café de especialidad, conocido por sus granos grandes, revela notas suaves de frutas tropicales y vainilla, con una acidez brillante y cuerpo sedoso. Cultivado con dedicación artesanal, cada taza cuenta una historia única de la región costera del Pacífico colombiano."
-            tostion: "Media baja"
-            gramos: ""
-            cantidad: ""
-            precio: "35.000"
-     }
+            nombre: "Maragogipe" ,
+            descripcion: "Desde las costas montañosas de Tumaco, Nariño, el Café Finca Los Negros ofrece un distinguido Maragogipe. Este café de especialidad, conocido por sus granos grandes, revela notas suaves de frutas tropicales y vainilla, con una acidez brillante y cuerpo sedoso. Cultivado con dedicación artesanal, cada taza cuenta una historia única de la región costera del Pacífico colombiano.",
+            tostion: "Media baja",
+            gramos: "",
+            cantidad: "",
+            precio: "35.000",
+            imagen: "",
+     },
 
         cafe2: { 
-            nombre: "Bourbon Natural"
-            descripcion: "Cultivado en las verdes tierras de Cerrito, Valle, el Café Finca El Burrito presenta un delicioso Bourbon Natural. Este café se caracteriza por sus notas dulces de frutas maduras y chocolate, con un cuerpo suave y una acidez agradable. Producido de manera artesanal, captura la esencia de la región vallecaucana en cada taza."
-            tostion: "Media Alta"
-            gramos: ""
-            cantidad: ""
-            precio: "25.000"
-     }
+            nombre: "Bourbon Natural",
+            descripcion: "Cultivado en las verdes tierras de Cerrito, Valle, el Café Finca El Burrito presenta un delicioso Bourbon Natural. Este café se caracteriza por sus notas dulces de frutas maduras y chocolate, con un cuerpo suave y una acidez agradable. Producido de manera artesanal, captura la esencia de la región vallecaucana en cada taza.",
+            tostion: "Media Alta",
+            gramos: "",
+            cantidad: "",
+            precio: "25.000",
+            imagen: "",
+     },
 
         cafe3: { 
-            nombre: "Geisha Lavado"
-            descripcion: "Cultivado en las montañas de Rionegro, Antioquia, el Café Finca "La Abuela" presenta un exquisito Geisha Lavado. Con delicadas notas florales y cítricas, este café destaca por su suavidad y refinada acidez. Producido de manera artesanal, cada taza captura la esencia única de su origen en las tierras antioqueñas."
-            tostion: "media"
-            gramos: ""
-            cantidad: ""
-            precio: "35.000"
-    }
+            nombre: "Geisha Lavado",
+            descripcion: 'Cultivado en las montañas de Rionegro, Antioquia, el Café Finca "La Abuela" presenta un exquisito Geisha Lavado. Con delicadas notas florales y cítricas, este café destaca por su suavidad y refinada acidez. Producido de manera artesanal, cada taza captura la esencia única de su origen en las tierras antioqueñas.',
+            tostion: "media",
+            gramos: "",
+            cantidad: "",
+            precio: "35.000",
+            imagen: "",
+    },
 
         cafe4: { 
-            nombre: "Tapi Natural"
-            descripcion: ""
-            tostion: ""
-            gramos: ""
-            cantidad: "" 
-            precio: "45.000"
-    }
+            nombre: "Tapi Natural",
+            descripcion: "",
+            tostion: "",
+            gramos: "",
+            cantidad: "" ,
+            precio: "45.000",
+            imagen: "",
+    },
 
         cafe5: { 
-            nombre: "Castillo Lavado"
-            descripcion: "Desde las colinas de Jamundí, Valle, Finca El Cafetero ofrece un exquisito Típica Natural. Con un perfil balanceado y notas de frutas tropicales y almendra, este café destaca por su suavidad y su carácter único. Cultivado con prácticas sostenibles, cada taza refleja la riqueza de su origen vallecaucano."
-            tostion: "Alta"
-            gramos: ""
-            cantidad: "" 
-            precio: "25.000"
-    }
+            nombre: "Castillo Lavado",
+            descripcion: "Desde las colinas de Jamundí, Valle, Finca El Cafetero ofrece un exquisito Típica Natural. Con un perfil balanceado y notas de frutas tropicales y almendra, este café destaca por su suavidad y su carácter único. Cultivado con prácticas sostenibles, cada taza refleja la riqueza de su origen vallecaucano.",
+            tostion: "Alta",
+            gramos: "",
+            cantidad: "" ,
+            precio: "25.000",
+            imagen: "",
+    },
 
         cafe6: { 
-            nombre: "Caturra Natural"
-            descripcion: "Cultivado en Pitalito, Huila, el Café Finca Burrito es un café de especialidad de variedad Caturra Natural. Con notas de frutas rojas, cítricos y chocolate oscuro, este café ofrece un perfil vibrante y equilibrado. Producido de manera artesanal y sostenible, captura lo mejor de las tierras altas del Huila."
-            tostion: "Alta"
-            gramos: ""
-            cantidad: ""
-            precio: "25.000"
-    }    
-}
+            nombre: "Caturra Natural",
+            descripcion: "Cultivado en Pitalito, Huila, el Café Finca Burrito es un café de especialidad de variedad Caturra Natural. Con notas de frutas rojas, cítricos y chocolate oscuro, este café ofrece un perfil vibrante y equilibrado. Producido de manera artesanal y sostenible, captura lo mejor de las tierras altas del Huila.",
+            tostion: "Alta",
+            gramos: "",
+            cantidad: "",
+            precio: "25.000",
+            imagen: "",
+    } ,   
+},
 
     Otros: { 
         producto1: { 
-            nombre: "Aeropress"
-            descripcion: "El Aeropress es un dispositivo compacto y versátil para preparar café. Permite ajustar temperatura, tiempo de extracción y presión, resultando en una taza suave y rica en matices. Ideal para casa o llevar de viaje, es fácil de usar y limpiar, perfecto para los amantes del café que buscan calidad en cada sorbo."
-            cantidad: ""
-            precio: "300.000"
-    }
+            nombre: "Aeropress",
+            descripcion: "El Aeropress es un dispositivo compacto y versátil para preparar café. Permite ajustar temperatura, tiempo de extracción y presión, resultando en una taza suave y rica en matices. Ideal para casa o llevar de viaje, es fácil de usar y limpiar, perfecto para los amantes del café que buscan calidad en cada sorbo.",
+            cantidad: "",
+            precio: "300.000",
+            imagen: "",
+    },
 
         producto2: { 
-            nombre: "Chemex"
-            descripcion: "El Chemex es un elegante y clásico método de preparación de café por filtración. Con su diseño de vidrio y forma distintiva, permite una extracción limpia y suave, resaltando los sabores y aromas del café. Ideal para quienes buscan una experiencia de café pura y estética, el Chemex es perfecto para preparar múltiples tazas a la vez, convirtiendo la rutina del café en un ritual sofisticado."
-            cantidad: ""
-            precio: "150.000"
-    }
+            nombre: "Chemex",
+            descripcion: "El Chemex es un elegante y clásico método de preparación de café por filtración. Con su diseño de vidrio y forma distintiva, permite una extracción limpia y suave, resaltando los sabores y aromas del café. Ideal para quienes buscan una experiencia de café pura y estética, el Chemex es perfecto para preparar múltiples tazas a la vez, convirtiendo la rutina del café en un ritual sofisticado.",
+            cantidad: "",
+            precio: "150.000",
+            imagen: "",
+    },
 
         producto3: { 
-            nombre: "Turkish"
-            descripcion: "El Cezve o turkish es un pequeño recipiente tradicional utilizado para preparar café turco. Fabricado típicamente de cobre o acero inoxidable, permite calentar agua y café finamente molido a fuego lento, creando una bebida intensa y espesa. Su diseño incluye un mango largo para facilitar el vertido y, a menudo, una forma distintiva que ayuda a formar la característica espuma del café turco. Ideal para quienes buscan una experiencia auténtica y cultural en la preparación del café."
-            cantidad: ""
-            precio: "75.000"
-    }
+            nombre: "Turkish",
+            descripcion: "El Cezve o turkish es un pequeño recipiente tradicional utilizado para preparar café turco. Fabricado típicamente de cobre o acero inoxidable, permite calentar agua y café finamente molido a fuego lento, creando una bebida intensa y espesa. Su diseño incluye un mango largo para facilitar el vertido y, a menudo, una forma distintiva que ayuda a formar la característica espuma del café turco. Ideal para quienes buscan una experiencia auténtica y cultural en la preparación del café.",
+            cantidad: "",
+            precio: "75.000",
+            imagen: "",
+    },
 
         producto4: { 
-            nombre: "Moka"
-            descripcion: "La cafetera Moka es un clásico método de preparación de café que utiliza vapor para extraer sabores intensos. Con su diseño icónico de aluminio o acero inoxidable, se compone de tres partes: un compartimento para agua, otro para café molido y un recipiente superior para el café listo. Al calentar, el vapor presiona el agua hacia arriba, produciendo un café fuerte y aromático, perfecto para quienes disfrutan de un espresso en casa sin complicaciones."
-            cantidad: ""
-            precio: "30.000"
-    }
+            nombre: "Moka",
+            descripcion: "La cafetera Moka es un clásico método de preparación de café que utiliza vapor para extraer sabores intensos. Con su diseño icónico de aluminio o acero inoxidable, se compone de tres partes: un compartimento para agua, otro para café molido y un recipiente superior para el café listo. Al calentar, el vapor presiona el agua hacia arriba, produciendo un café fuerte y aromático, perfecto para quienes disfrutan de un espresso en casa sin complicaciones.",
+            cantidad: "",
+            precio: "30.000",
+            imagen: "",
+    },
 
         producto5: { 
-            nombre: "Filtros V60"
-            descripcion: "Los filtros V60 son accesorios diseñados específicamente para el método de preparación de café V60. Hechos de papel, estos filtros permiten una extracción uniforme, realzando los sabores y aromas del café. Su diseño cónico y sus ranuras laterales facilitan el flujo del agua, garantizando una taza limpia y equilibrada. Ideales para quienes buscan un café de calidad y un proceso de elaboración simple y efectivo."
-            cantidad: ""
-            precio: "15.000"
-    }
+            nombre: "Filtros V60",
+            descripcion: "Los filtros V60 son accesorios diseñados específicamente para el método de preparación de café V60. Hechos de papel, estos filtros permiten una extracción uniforme, realzando los sabores y aromas del café. Su diseño cónico y sus ranuras laterales facilitan el flujo del agua, garantizando una taza limpia y equilibrada. Ideales para quienes buscan un café de calidad y un proceso de elaboración simple y efectivo.",
+            cantidad: "",
+            precio: "15.000",
+            imagen: "",
+    },
 
         producto6: { 
-            nombre: "Taza Cafe"
-            descripcion:"Esta taza de café artesanal combina diseño moderno y funcionalidad. Hecha de cerámica en tonos beige, su acabado suave y textura natural la hacen perfecta para disfrutar de tu café favorito. El asa ergonómica proporciona un agarre cómodo, mientras que el plato de madera complementa el conjunto, añadiendo un toque rústico y elegante a cualquier mesa. Ideal para quienes aprecian tanto el sabor del café como la estética de su presentación."
-            cantidad: 
-            precio: "25.000"
-    }   
+            nombre: "Taza Cafe",
+            descripcion:"Esta taza de café artesanal combina diseño moderno y funcionalidad. Hecha de cerámica en tonos beige, su acabado suave y textura natural la hacen perfecta para disfrutar de tu café favorito. El asa ergonómica proporciona un agarre cómodo, mientras que el plato de madera complementa el conjunto, añadiendo un toque rústico y elegante a cualquier mesa. Ideal para quienes aprecian tanto el sabor del café como la estética de su presentación.",
+            cantidad: "",
+            precio: "25.000",
+            imagen: "",
+    } ,  
 
         producto7: { 
-            nombre: "Sifon Japones"
-            descripcion: "El sifón japonés es un elegante y sofisticado método de preparación de café que utiliza el principio de vacío. Con su diseño de vidrio en forma de bomba, permite una extracción precisa y teatral, revelando sabores complejos y aromas intensos. Este proceso involucra calentar agua en la cámara inferior, lo que crea vapor que empuja el agua hacia la parte superior, donde se mezcla con el café molido. Una vez que el fuego se apaga, el vacío generado atrae el café de vuelta a la cámara inferior, resultando en una taza limpia y aromática. Ideal para los amantes del café que buscan una experiencia única y visual."
-            cantidad: ""
-            precio: "350.000"
-    }
+            nombre: "Sifon Japones",
+            descripcion: "El sifón japonés es un elegante y sofisticado método de preparación de café que utiliza el principio de vacío. Con su diseño de vidrio en forma de bomba, permite una extracción precisa y teatral, revelando sabores complejos y aromas intensos. Este proceso involucra calentar agua en la cámara inferior, lo que crea vapor que empuja el agua hacia la parte superior, donde se mezcla con el café molido. Una vez que el fuego se apaga, el vacío generado atrae el café de vuelta a la cámara inferior, resultando en una taza limpia y aromática. Ideal para los amantes del café que buscan una experiencia única y visual.",
+            cantidad: "",
+            precio: "350.000",
+            imagen: "",
+    },
 
         producto8: { 
-            nombre: "Maquina Espresso"
-            descripcion: "La máquina de espresso es un equipo esencial para los amantes del café que desean disfrutar de un espresso de calidad en casa. Con su capacidad para generar alta presión, extrae los sabores concentrados de los granos de café, creando una bebida rica y cremosa. Ideal para preparar diversos tipos de café, como cappuccinos y lattes, su diseño varía desde modelos manuales hasta automáticos, adaptándose a diferentes niveles de experiencia. Con una máquina de espresso, cada taza se convierte en una obra maestra del café."
-            cantidad: ""
-            precio: "950.000"
-    }
+            nombre: "Maquina Espresso",
+            descripcion: "La máquina de espresso es un equipo esencial para los amantes del café que desean disfrutar de un espresso de calidad en casa. Con su capacidad para generar alta presión, extrae los sabores concentrados de los granos de café, creando una bebida rica y cremosa. Ideal para preparar diversos tipos de café, como cappuccinos y lattes, su diseño varía desde modelos manuales hasta automáticos, adaptándose a diferentes niveles de experiencia. Con una máquina de espresso, cada taza se convierte en una obra maestra del café.",
+            cantidad: "",
+            precio: "950.000",
+            imagen: "",
+    },
 
         producto9: { 
-            nombre: "Filtro Aeropress"
-            descripcion:"Los filtros para Aeropress son accesorios esenciales para preparar café de manera sencilla y eficaz. Hechos de papel o metal, estos filtros permiten una filtración óptima, evitando sedimentos y asegurando una taza suave y limpia. Su diseño específico complementa el método de extracción de la Aeropress, realzando los sabores y aromas del café. Con estos filtros, disfrutar de un café de calidad en casa nunca ha sido tan fácil."
-            cantidad: ""
-            precio: "20.000"
-    }
+            nombre: "Filtro Aeropress",
+            descripcion:"Los filtros para Aeropress son accesorios esenciales para preparar café de manera sencilla y eficaz. Hechos de papel o metal, estos filtros permiten una filtración óptima, evitando sedimentos y asegurando una taza suave y limpia. Su diseño específico complementa el método de extracción de la Aeropress, realzando los sabores y aromas del café. Con estos filtros, disfrutar de un café de calidad en casa nunca ha sido tan fácil.",
+            cantidad: "",
+            precio: "20.000",
+            imagen: "",
+    },
 
         producto10: { 
-            nombre: "Molino Cafe"
-            descripcion: "Este molino de café manual combina diseño elegante y funcionalidad. Fabricado con materiales de alta calidad, presenta un cuerpo metálico y un detalle de madera que añade un toque de calidez y sofisticación. Su mecanismo de molienda ajustable permite obtener granos de café con la finura ideal para cualquier método de preparación, desde espresso hasta prensa francesa. Compacto y portátil, es perfecto para disfrutar de café fresco en casa o llevarlo de viaje. Con este molino, cada taza de café se convierte en una experiencia única y personalizada."
-            cantidad: ""
-            precio: "80.000"
-    }   
-    }
+            nombre: "Molino Cafe",
+            descripcion: "Este molino de café manual combina diseño elegante y funcionalidad. Fabricado con materiales de alta calidad, presenta un cuerpo metálico y un detalle de madera que añade un toque de calidez y sofisticación. Su mecanismo de molienda ajustable permite obtener granos de café con la finura ideal para cualquier método de preparación, desde espresso hasta prensa francesa. Compacto y portátil, es perfecto para disfrutar de café fresco en casa o llevarlo de viaje. Con este molino, cada taza de café se convierte en una experiencia única y personalizada.",
+            cantidad: "",
+            precio: "80.000",
+            imagen: "",
+    }   ,
+    },
 }
 
