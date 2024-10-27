b7187edaed5acec534bb7ed81e71ca1b8919a2c3
CrisJauregui <cristinajaureguibravo@gmail.com>
2024-10-27 17:55:08 -0500

    avances

--- Info User.js
+++ Info User.js
@@ -3,35 +3,33 @@ document.addEventListener('DOMContentLoaded', () => {
     const editValueInput = document.getElementById('editValue');
     const closeModal = document.getElementById('closeModal');
     const updateForm = document.getElementById('updateForm');
-    let currentItem; // Variable para rastrear el elemento actual en edición
+    let currentItem; 
 
-    // Abrir el formulario de edición al hacer clic en "EDITAR"
+    
     document.querySelectorAll('.profile-item button').forEach(button => {
         button.addEventListener('click', (event) => {
             currentItem = event.target.previousElementSibling;
-            editValueInput.value = currentItem.innerText; // Prellenar el campo con el valor actual
+            editValueInput.value = currentItem.innerText; 
             editForm.style.display = 'flex';
         });
     });
 
-    // Cerrar el formulario de edición
     closeModal.addEventListener('click', () => {
         editForm.style.display = 'none';
     });
 
-    // Guardar el valor actualizado
+ 
     updateForm.addEventListener('submit', (event) => {
         event.preventDefault();
         const newValue = editValueInput.value.trim();
         if (newValue) {
-            currentItem.innerText = newValue; // Actualizar el valor en la interfaz de usuario
-            editForm.style.display = 'none'; // Cerrar el formulario emergente
+            currentItem.innerText = newValue; 
+            editForm.style.display = 'none'; 
         } else {
             alert('El campo no puede estar vacío');
         }
     });
 
-    // Cerrar el formulario emergente al hacer clic fuera del mismo
     window.addEventListener('click', (event) => {
         if (event.target === editForm) {
             editForm.style.display = 'none';
